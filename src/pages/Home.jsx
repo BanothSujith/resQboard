98d6563f2c0a5c98dc2d8188c611897aa6b0c87e
BanothSujith <banothsujith4@gmail.com>
2025-07-14 23:51:40 +0530

    added location symbol

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,16 @@ import "leaflet/dist/leaflet.css";
 import { FcHome } from "react-icons/fc";
 import { ImLocation2 } from "react-icons/im";
 
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png",
+  iconUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png",
+  shadowUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png",
+});
+
 const Home = () => {
   const [location, setLocation] = useState(null);
   const [network, setNetwork] = useState(null);
